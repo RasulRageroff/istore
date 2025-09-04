7e0c9548cf49bf9103552885c0944be73b55e66c
Rageroff <rasul.k.k@mail.ru>
2025-09-04 22:19:03 +0500

    Создать веб приложение :

--- infastructure/Store.Memory/BookRepository.cs
+++ infastructure/Store.Memory/BookRepository.cs
@@ -1,12 +1,21 @@
 ﻿using istore;
+using System.Linq;
 
 namespace Store.Memory
 {
     public class BookRepository : IBookRepository
     {
+
+
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language"),
+        };
         public Book[] GetAllByTitle(string titlePart)
         {
-            throw new NotImplementedException();
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
         }
     }
 }
